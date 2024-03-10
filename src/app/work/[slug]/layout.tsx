0a1d58079d4d1620dc0b50613aba08f7c4ce07f0
Elliot Saha <elliotsaha@gmail.com>
2024-03-09 19:59:25 -0800

    feat(slug): removed axios request on layout for slugs

--- src/app/work/[slug]/layout.tsx
+++ src/app/work/[slug]/layout.tsx
@@ -1,32 +1,17 @@
-import type {Metadata} from 'next';
-import type {CaseStudy} from '@/types';
+import type { Metadata } from 'next';
 
 export async function generateMetadata({
   params,
 }: {
-  params: {slug: string};
+  params: { slug: string };
 }): Promise<Metadata> {
-  const caseStudy: CaseStudy = await fetch(
-    `${process.env.NEXT_PUBLIC_HOSTNAME}/api/work/caseStudies/slug`,
-    {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({slug: params.slug}),
-    }
-  ).then(res => res.json());
+  const name = params.slug.split("-").map(i => i.charAt(0).toUpperCase() + i.slice(1)).join(" ")
 
   return {
-    title: `${caseStudy.title} | Elliot Saha's Portfolio`,
-    description: caseStudy.shortDescription,
-    openGraph: {
-      images: [caseStudy.cover, caseStudy.icon],
-    },
+    title: `${name} | Elliot Saha's Portfolio`,
   };
 }
 
-const Layout = ({children}: {children: React.ReactNode}) => children;
+const Layout = ({ children }: { children: React.ReactNode }) => children;
 
 export default Layout;
